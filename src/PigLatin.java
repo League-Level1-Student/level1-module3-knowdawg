302a65ca9000193b9e33e6d36ee032cbe22267d9
league <league@wts8.attlocal.net>
2018-08-18 11:28:01 -0700

    whack a mole

--- src/PigLatin.java
+++ src/PigLatin.java
@@ -11,8 +11,8 @@ public class PigLatin {
 
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
-	JButton button = new JButton();
-	JTextField field1 = new JTextField(10);
+	JButton button = new JButton("Translate");
+	JTextField field = new JTextField(10);
 	JLabel label = new JLabel();
 	
 	
@@ -25,12 +25,23 @@ public class PigLatin {
 	void setUp() {
 		
 		frame.add(panel);
-		panel.add(field1);
+		panel.add(field);
 		panel.add(button);
 		panel.add(label);
 		frame.setVisible(true);
 		frame.pack();
 		
+		PigLatinTranslator pigy = new PigLatinTranslator();
+		
+		String answer = pigy.translate(field.getText());
+		
+		label.setText("" + answer);
+		System.out.println("" + answer);
+		
 	}
+	
+	
+
+
 
 }
